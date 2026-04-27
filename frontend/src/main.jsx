4cbf02b09d3f171e7db69a66c4008efcd60346c9
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-04-27 21:55:09 +0200

    fix bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import FinaleEvaluatie from "./pages/FinaleEvaluatieMentor.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/finale-evaluatie" />} />
+        <Route path="/finale-evaluatie" element={<FinaleEvaluatie />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);

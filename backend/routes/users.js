772a13a65f2303b0d316190c39d2993eca6d11de
Ty0906 <tiffany@binnebeek.be>
2026-04-27 19:50:17 +0200

    feat(admin-users): complete CRUD, validation and password reset

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,5 +5,9 @@ const userController = require("../controllers/userController");
 
 router.get("/", userController.getAllUsers);
 router.post("/", userController.createUser);
+router.delete("/:id", userController.deleteUser);
+router.put("/:id", userController.updateUser);
+router.put("/:id/password", userController.resetPassword);
+
 
 module.exports = router;

cea670d938a1aa01bcde82d154e73b1a4f54dcd9
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-04-28 11:35:16 +0200

    route main naar docent

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/finale-evaluatie" />} />
+     <Route path="/" element={<Navigate to="/finale-evaluatie-docent" />} />
         <Route path="/finale-evaluatie" element={<FinaleEvaluatie />} />
         <Route path="/finale-evaluatie-docent" element={<FinaleEvaluatieDocent />} />
       </Routes>

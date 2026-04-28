231244a8058db77400e15f1b8d90daed0d9ae824
Ty0906 <tiffany@binnebeek.be>
2026-04-28 10:05:45 +0200

    feat(auth): add routing, dashboards and translations

---
+++ frontend/src/pages/InternshipCommitteeDashboard.jsx
@@ -0,0 +1,5 @@
+import { t } from "../i18n/translations";
+
+export default function InternshipCommitteeDashboard() {
+  return <h1>{t("dashboards.internship_committee")}</h1>;
+}
---
+++ frontend/src/pages/TeacherDashboard.jsx
@@ -0,0 +1,5 @@
+import { t } from "../i18n/translations";
+
+export default function TeacherDashboard() {
+  return <h1>{t("dashboards.teacher")}</h1>;
+}

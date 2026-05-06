e5f06c11a8206e27fb3c4647e8bd70dc8e541c9c
karenvandenheuvel-arch <karen.vandenheuvel@hotmail.com>
2026-05-06 12:42:58 +0200

    WIP: update App.jsx voor nieuwe route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./components/Login";
 
-
 import StudentDashboard from "./pages/StudentDashboard";
 import TeacherDashboard from "./pages/TeacherDashboard";
 import MentorDashboard from "./pages/MentorDashboard";
@@ -12,6 +11,9 @@ import AdminUsers from "./pages/AdminUsers";
 import AdminInternshipDetail from "./pages/AdminInternshipDetail";
 import StudentInternshipDetail from "./pages/StudentInternshipDetail";
 
+// ⭐ Nieuwe import voor jouw nieuwe pagina
+import NewInternshipRequest from "./pages/NewInternshipRequest";
+
 function App() {
   return (
     <BrowserRouter>
@@ -19,14 +21,15 @@ function App() {
 
         <Route path="/" element={<Navigate to="/login" />} />
 
-
         <Route path="/login" element={<Login />} />
 
-
-
-
-
         <Route path="/dashboard/student" element={<StudentDashboard />} />
+        
+        {/* ⭐ Nieuwe route voor nieuwe stageaanvraag */}
+        <Route path="/student/new-request" element={<NewInternshipRequest />} />
+
+        <Route path="/student/internships/:id" element={<StudentInternshipDetail />} />
+
         <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
         <Route path="/dashboard/mentor" element={<MentorDashboard />} />
         <Route
@@ -36,11 +39,7 @@ function App() {
         <Route path="/dashboard/admin" element={<AdminDashboard />} />
 
         <Route path="/admin/users" element={<AdminUsers />} />
-
         <Route path="/admin/internships/:id" element={<AdminInternshipDetail />} />
-    
-
-        <Route path="/student/internships/:id" element={<StudentInternshipDetail />} />
 
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>

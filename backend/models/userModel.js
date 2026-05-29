7fd7a566f0ec9188c81a6dd2b47b4c543919fff6
Anicée <sobacassou@protonmail.com>
2026-05-29 09:23:49 +0200

    Revert "Feature/dashbord docent"

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,11 @@
 const db = require("../db");
 
 module.exports = {
-
-  getAllUsers(role, callback) {
-    let query = `
-      SELECT
-        users.id,
-        users.firstname,
-        users.lastname,
-        users.email,
-        users.username,
-        users.role,
-        users.status,
-        students.studyprogram
-      FROM users
-      LEFT JOIN students ON students.user_id = users.id
-    `;
-
-    const params = [];
-
-    if (role) {
-      query += " WHERE users.role = ?";
-      params.push(role);
-    }
-
-    db.query(query, params, callback);
+  getAllUsers(callback) {
+    db.query(
+      "SELECT id, firstname, lastname, email, username, role, status FROM users",
+      callback
+    );
   },
 
   createUser(user, callback) {
@@ -49,62 +30,5 @@ module.exports = {
 
   getUserByUsername(username, callback) {
     db.query("SELECT * FROM users WHERE username = ?", [username], callback);
-  },
-
-  getUserByEmailExceptId(email, id, callback) {
-    db.query(
-      "SELECT id FROM users WHERE email = ? AND id != ?",
-      [email, id],
-      callback
-    );
-  },
-
-  getUserByUsernameExceptId(username, id, callback) {
-    db.query(
-      "SELECT id FROM users WHERE username = ? AND id != ?",
-      [username, id],
-      callback
-    );
-  },
-
-  deleteUser(id, callback) {
-    db.query(
-      "DELETE FROM users WHERE id = ?",
-      [id],
-      callback
-    );
-  },
-
-  updateUser(id, user, callback) {
-    db.query(
-      `
-        UPDATE users
-        SET firstname = ?,
-            lastname = ?,
-            email = ?,
-            username = ?,
-            role = ?,
-            status = ?
-        WHERE id = ?
-      `,
-      [
-        user.firstname,
-        user.lastname,
-        user.email,
-        user.username,
-        user.role,
-        user.status,
-        id
-      ],
-      callback
-    );
-  },
-
-  updatePassword(id, hashedPassword, callback) {
-    db.query(
-      "UPDATE users SET password = ? WHERE id = ?",
-      [hashedPassword, id],
-      callback
-    );
   }
 };
